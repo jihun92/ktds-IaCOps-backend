4632a2bb3646fd82a05d460d995a1ac2c1d71d8d
jihun92 <jihunkim921114@gmail.com>
2023-05-04 01:22:25 +0900

    메인 클레스 테스트용 주석 삭제

--- src/test/java/com/ktds/IaCOps/IaCOpsApplicationTests.java
+++ src/test/java/com/ktds/IaCOps/IaCOpsApplicationTests.java
@@ -24,10 +24,28 @@ class IaCOpsApplicationTests {
 			ansibleService.setHost(inven);
 			ansibleService.selectPlaybook("debugMsg.ymal");
 			List<String> rs = ansibleService.runPlaybook();
-				
+
 			assertEquals(rs, 1);
 	
 			System.out.println(rs);
+
+
+//		// yml test
+//		YamlService yamlService = new YamlService();
+//		String rootPath = System.getProperty("user.dir");
+//		String filePath = rootPath+"/src/main/resources/";
+//		String fileName = "application.properties";
+//
+//		Map<String, Object> yamlData = yamlService.readYaml(filePath, fileName);
+////		System.out.println(yamlData);
+//
+//		yamlService.writeYaml(filePath, fileName,"server.port", 8080, yamlData);
+//
+//
+//		// cli TEST
+//		CliService cli = new CliService();
+//		List<String> out = cli.runCommand("ls -all");
+//		System.out.println(out);
 
 	}
 
