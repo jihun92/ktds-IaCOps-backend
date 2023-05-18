271f96d7f47ca91a7779d63ed05dc6b8485df8a1
jihun92 <jihunkim921114@gmail.com>
2023-05-18 10:23:01 +0900

    gitService -> gitComponent 로 변경 및 import 정리

--- src/main/java/com/ktds/IaCOps/common/vcs/gitlab/component/GitComponent.java
+++ src/main/java/com/ktds/IaCOps/common/vcs/gitlab/component/GitComponent.java
@@ -1,38 +1,33 @@
 package com.ktds.IaCOps.common.vcs.gitlab.service;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+
 import org.eclipse.jgit.api.CloneCommand;
-import org.eclipse.jgit.api.PullCommand;
-import org.eclipse.jgit.api.AddCommand;
 import org.eclipse.jgit.api.CommitCommand;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.PullCommand;
+import org.eclipse.jgit.api.PullResult;
 import org.eclipse.jgit.api.PushCommand;
 import org.eclipse.jgit.api.ResetCommand;
-import org.eclipse.jgit.api.PullResult;
-import org.eclipse.jgit.api.RevertCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Ref;
-import java.util.Scanner;
+import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
-import java.io.IOException;
-
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.ResetCommand.ResetType;
-import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.transport.PushResult;
 import org.eclipse.jgit.transport.RemoteRefUpdate;
-import org.eclipse.jgit.transport.URIish;
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
-import org.springframework.stereotype.Service;
-import java.io.File;
-import java.util.Collection;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 
 
-@Service
-public class GitService {
+@Component
+public class GitComponent {
 	@Value("${vcs.gitlab.url}")
 	private String gitlabUrl;
 	@Value("${vcs.gitlab.username}")
