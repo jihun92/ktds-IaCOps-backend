d7217f27713468d9252d39d71af76a13387ece93
jihun92 <jihunkim921114@gmail.com>
2023-05-30 15:45:07 +0900

    IAC2-56 테라폼 플랜, 어플리 명령어 수정

---
+++ src/main/java/com/ktds/IaCOps/iacengine/terraform/component/TerraformComponent.java
@@ -0,0 +1,29 @@
+package com.ktds.IaCOps.iacengine.terraform.component;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.ktds.IaCOps.common.cli.CliService;
+
+@Component
+public class TerraformComponent {
+    
+	// @Value("${filePath.infra_code.infra_config.path}")
+	// private String infra_code_path;
+
+	@Autowired
+	CliService cli;
+
+	public List<String> plan() {
+		String runCommand = "sudo terraform -chdir=/var/lib/iacops/iacops-project/terraform/ plan";
+		return cli.runCommand(runCommand);
+	}
+
+    public List<String> apply() {
+		String runCommand = "sudo terraform -chdir=/var/lib/iacops/iacops-project/terraform/ apply";
+		return cli.runCommand(runCommand);
+	}
+
+}
